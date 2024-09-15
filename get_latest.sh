bdeac31af06ff4a577ab1ca4de1c9a0d93a54cb4
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2024-09-15 12:34:46 +0200

    Add script to update externals.

---
+++ get_latest.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+set -xe
+
+cp ../../raysan5/raylib/src/external/cgltf.h ./external
+cp ../../raysan5/raylib/src/external/dirent.h ./external
+cp ../../raysan5/raylib/src/external/dr_flac.h ./external
+cp ../../raysan5/raylib/src/external/dr_mp3.h ./external
+cp ../../raysan5/raylib/src/external/dr_wav.h ./external
+cp ../../raysan5/raylib/src/external/glad_gles2.h ./external
+cp ../../raysan5/raylib/src/external/glad.h ./external
+cp ../../raysan5/raylib/src/external/jar_mod.h ./external
+cp ../../raysan5/raylib/src/external/jar_xm.h ./external
+cp ../../raysan5/raylib/src/external/m3d.h ./external
+cp ../../raysan5/raylib/src/external/miniaudio.h ./external
+cp ../../raysan5/raylib/src/external/msf_gif.h ./external
+cp ../../raysan5/raylib/src/external/nanosvg.h ./external
+cp ../../raysan5/raylib/src/external/nanosvgrast.h ./external
+cp ../../raysan5/raylib/src/external/par_shapes.h ./external
+cp ../../raysan5/raylib/src/external/qoa.h ./external
+cp ../../raysan5/raylib/src/external/qoaplay.c ./external
+cp ../../raysan5/raylib/src/external/qoi.h ./external
+cp ../../raysan5/raylib/src/external/RGFW.h ./external
+cp ../../raysan5/raylib/src/external/rl_gputex.h ./external
+cp ../../raysan5/raylib/src/external/rprand.h ./external
+cp ../../raysan5/raylib/src/external/sdefl.h ./external
+cp ../../raysan5/raylib/src/external/sinfl.h ./external
+cp ../../raysan5/raylib/src/external/stb_image.h ./external
+cp ../../raysan5/raylib/src/external/stb_image_resize2.h ./external
+cp ../../raysan5/raylib/src/external/stb_image_write.h ./external
+cp ../../raysan5/raylib/src/external/stb_perlin.h ./external
+cp ../../raysan5/raylib/src/external/stb_rect_pack.h ./external
+cp ../../raysan5/raylib/src/external/stb_truetype.h ./external
+cp ../../raysan5/raylib/src/external/stb_vorbis.c ./external
+cp ../../raysan5/raylib/src/external/tinyobj_loader_c.h ./external
+cp ../../raysan5/raylib/src/external/vox_loader.h ./external
+
+
+cp ../../raysan5/raylib/src/config.h ./external/raylib
+cp ../../raysan5/raylib/src/platforms/*.c ./external/raylib/platforms
+cp ../../raysan5/raylib/src/raudio.c ./external/raylib
+cp ../../raysan5/raylib/src/raylib.h ./external/raylib
+cp ../../raysan5/raylib/src/raymath.h ./external/raylib
+cp ../../raysan5/raylib/src/rcamera.h ./external/raylib
+cp ../../raysan5/raylib/src/rcore.c ./external/raylib
+cp ../../raysan5/raylib/src/rgestures.h ./external/raylib
+cp ../../raysan5/raylib/src/rglfw.c ./external/raylib
+cp ../../raysan5/raylib/src/rlgl.h ./external/raylib
+cp ../../raysan5/raylib/src/rmodels.c ./external/raylib
+cp ../../raysan5/raylib/src/rshapes.c ./external/raylib
+cp ../../raysan5/raylib/src/rtext.c ./external/raylib
+cp ../../raysan5/raylib/src/rtextures.c ./external/raylib
+cp ../../raysan5/raylib/src/utils.c ./external/raylib
+cp ../../raysan5/raylib/src/utils.h ./external/raylib
